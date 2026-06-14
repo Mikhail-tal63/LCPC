66ef80a7c48266adb98fea0958bf3b76129e4d83
Jehad Mohamed <jjehad100@gmail.com>
2026-06-14 18:58:50 +0200

    ksenia

---
+++ Gewmetry/Alyona and Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main() {
+    long long n, m;
+    cin >> n >> m;
+    
+    long long ans = 0;
+    vector<long long> a(5,0),b(5,0);
+    for(int i =0; i < n; i++){
+
+    }
+
+    return 0;
+}
--- Gewmetry/Good Number.cpp
+++ Gewmetry/Good Number.cpp
@@ -1,47 +1,26 @@
 #include <iostream>
 #include <string>
-#include <cmath>
+#include <vector>
 using namespace std;
 
-int main()
-{
+int main() {
+int n,k,c=0;
+cin >> n >> k;
 
-    int n, k, c = 0;
+while(n--){
+    string s;
+    cin >> s;
+    vector<bool> found(10,false);
+
+    for(char c:s){
+        found[c-'0']= true;
+    }
 
     bool cool = true;
 
-    cin >> n >> k;
-    
-    string s, real = "1234567890";
-    int size = s.size();
-    for (int i = 0; i < n; i++)
-    {
-        cin >> s;
-        if(s.size() != k+1)
-        cool = true;
-        cout<< s[size-1];
-        if (s[size - 1] != 0){
-            cool = false;
-        }
-         
-        for (int j = 0; j < s.size() - 1; j++)
-        {
-            if (s[j] != real[j])
-                cool = false;
-        }
 
-        if (cool)
-        {
-            c++;
-        }
-    }
-    if (c ==0)
-    {
-        cout << -1;
-    }else
-    {
-        cout << c;
-    }
+    
+}
 
     return 0;
 }
